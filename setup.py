dc01e308d531d8a486af58f6d326532f6f6f4c41
MichaelE11 <electmike84@gmail.com>
2025-02-17 07:29:39 +0100

    requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="student_performance",
+    version="0.0.0",
+    author="Ephraim Michael",
+    author_email="electmike84@gmail.com",
+    packages=find_packages()
+)
