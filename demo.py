076e7bffdc22f524d180b48eb7cf18dd0575002f
MichaelE11 <electmike84@gmail.com>
2025-02-20 19:54:17 +0100

    utils

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
 from student_performance.logger import logging
+import sys
+from student_performance.exception import student_performanceException
+ 
 
 logging.info("welcome to custom logging")
+
+try:
+    2/0
+
+except Exception as e:
+    raise student_performanceException(e,sys)
